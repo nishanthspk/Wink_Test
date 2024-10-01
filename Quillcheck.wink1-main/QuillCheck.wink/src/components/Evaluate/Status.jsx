5be953ac7aa253cc25a6fbff98938b2d708a78ba
nishanthspk <spknishanth2004@gmail.com>
2024-10-01 15:20:54 +0530

    padding

--- Quillcheck.wink1-main/QuillCheck.wink/src/components/Evaluate/Status.jsx
+++ Quillcheck.wink1-main/QuillCheck.wink/src/components/Evaluate/Status.jsx
@@ -8,7 +8,7 @@ const Status = ({ totalScore, tokenAge }) => {
         <div className="space-y-2 lg:space-y-3">
           <div className="">
             <p className='text-[#DDDDDD]'>Last Known Status:</p>
-            <div className="bg-[#FFFFFF]/10 h-6 lg:h-10 w-16 lg:w-fit rounded-[8px] p-[2px] lg:p-[10px] flex items-center gap-1 lg:gap-2">
+            <div className="bg-[#FFFFFF]/10 h-6 lg:h-10 w-16 lg:w-fit rounded-[8px] p-[1px] lg:p-[10px] flex items-center gap-1 lg:gap-2">
               <img className='h-4 lg:h-6' src={Assets.Token} alt="" />
               <p className='text-[8px] lg:text-sm'>Not a <br />Honeypot</p>
             </div>
@@ -22,7 +22,7 @@ const Status = ({ totalScore, tokenAge }) => {
         <div className="space-y-3">
           <div className="">
             <p className='text-[#DDDDDD] '>Overall Score:</p>
-            <div className="bg-[#B40D00] h-5 lg:h-10 w-full rounded-[8px] flex justify-center items-center mx-auto">
+            <div className="bg-[#B40D00] h-5 lg:h-10 w-full rounded-[8px] flex justify-center items-center lg:mx-auto">
               {/* Display the totalScore prop */}
               <p className='text-base font-semibold'>{totalScore}%</p>
             </div>
