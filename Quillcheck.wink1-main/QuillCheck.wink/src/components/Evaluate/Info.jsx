a44f227edfd15de3c3e584c0c2e55dbc49d9253a
nishanthspk <spknishanth2004@gmail.com>
2024-10-01 20:43:56 +0530

    height6

--- Quillcheck.wink1-main/QuillCheck.wink/src/components/Evaluate/Info.jsx
+++ Quillcheck.wink1-main/QuillCheck.wink/src/components/Evaluate/Info.jsx
@@ -60,7 +60,7 @@ const Info = ({ holdersCount, currentLiquidity, lpHolders }) => {
                 <div className="">
                     <p>Transfer Tax</p>
                     <div className=" flex justify-center gap-1 items-center">
-                        <img className='lg:h-4' src={Assets.Shield} alt="" />
+                        <img className='h-2 lg:h-4' src={Assets.Shield} alt="" />
                         <p className=' font-semibold text-md'>0%</p>
                     </div>
                 </div>
